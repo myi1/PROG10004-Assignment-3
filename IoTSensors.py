2c8f8a5e9350e1002d0ca3360c8ec1306ad33131
Muhammad Ismail <yahyaismail@gmail.com>
2022-03-21 11:27:38 -0400

    IoTSensors.py: Define posX, posY, noDays, and avgRead attributes with accessor and mutator methods

--- IoTSensors.py
+++ IoTSensors.py
@@ -1,2 +1,31 @@
 class IoTSensors:
-    pass
+
+    def __init__(self):
+        self._posX = 0
+        self._posY = 0
+        self._noDays = 0
+        self._avgRead = 0
+
+    def getPosX(self):
+        return self._posX
+
+    def getPosY(self):
+        return self._posY
+
+    def getNoDays(self):
+        return self._noDays
+
+    def getAvgRead(self):
+        return self._avgRead
+
+    def setPosX(self, newPosX):
+        self._posX = newPosX
+
+    def setPosY(self, newPosY):
+        self._posY = newPosY
+
+    def setNoDays(self, newNoDays):
+        self._noDays = newNoDays
+
+    def setAvgRead(self, newAvgRead):
+        self._avgRead = newAvgRead
