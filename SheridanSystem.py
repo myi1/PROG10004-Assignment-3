2f50a66d2de0841296154758a59363d750105c99
Muhammad Ismail <yahyaismail@gmail.com>
2022-03-21 10:38:22 -0400

    SheridanSystem.py: Define noOfBuild attribute

--- SheridanSystem.py
+++ SheridanSystem.py
@@ -1,3 +1,10 @@
 
 class SheridanSystem:
-    print('test message')
+    def __init__(self):
+        self._noOfBuild = 0
+
+    def getNoOfBuild(self):
+        return self._noOfBuild
+
+    def setNoOfBuild(self, newNoOfBuild):
+        self._noOfBuild = newNoOfBuild
