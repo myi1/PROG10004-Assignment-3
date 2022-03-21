46819911296b474772cca7406f21e5dd8cc99638
Muhammad Ismail <yahyaismail@gmail.com>
2022-03-21 11:16:04 -0400

    Building.py: Definite noOfSensors, listofSensors, buildName attributes with accessors and mutator methods

--- Building.py
+++ Building.py
@@ -1,2 +1,24 @@
 class Building:
-    pass
+
+    def __init__(self):
+        self._noOfSensors = 0
+        self._listOfSensors = []
+        self._buildName = ''
+
+    def getNoOfSensors(self):
+        return self._noOfSensors
+
+    def getListOfSensors(self):
+        return self._listOfSensors
+
+    def getBuildName(self):
+        return self._buildName
+
+    def setnoOfSensors(self, newNoOfSensors):
+        self._noOfSensors = newNoOfSensors
+
+    def setListOfSensors(self, newListofSensors):
+        self._listOfSensors = newListofSensors
+
+    def setBuildName(self, newBuildName):
+        self._buildName = newBuildName
