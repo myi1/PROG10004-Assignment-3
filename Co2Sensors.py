f8a805c8b8485aa07d4295bd545b54e2c2a53782
Muhammad Ismail <yahyaismail@gmail.com>
2022-03-21 11:42:09 -0400

    Co2Sensors.py: Define sensorPos method

--- Co2Sensors.py
+++ Co2Sensors.py
@@ -1,7 +1,13 @@
 from IoTSensors import IoTSensors
+import random
 
 
 class Co2Sensors(IoTSensors):
 
     def __init__(self):
+        super.__init__()
         self._co2Levels = 0
+
+    def sensorPos(self):
+        self._posX = format(random.uniform(0, 100), '.2f')
+        self._posY = format(random.uniform(0, 100), '.2f')
