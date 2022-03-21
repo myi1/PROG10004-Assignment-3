16ad081cef2a2bf35f17b16bde0557a02e9b1160
Muhammad Ismail <yahyaismail@gmail.com>
2022-03-21 12:01:29 -0400

    Co2Sensors.py: Definite sensorReadings method

--- Co2Sensors.py
+++ Co2Sensors.py
@@ -8,6 +8,18 @@ class Co2Sensors(IoTSensors):
         super.__init__()
         self._co2Levels = 0
 
+    def getco2Levels(self):
+        return self._co2Levels
+
+    def setCo2Levels(self, newCo2Levels):
+        self._co2Levels = newCo2Levels
+
     def sensorPos(self):
         self._posX = format(random.uniform(0, 100), '.2f')
         self._posY = format(random.uniform(0, 100), '.2f')
+
+    def sensorReadings(self, sensorNum):
+        dayReading = input(
+            'Enter the CO2 Reading (PPM) for Day {0}'.format(sensorNum))
+        if dayReading.isdigit():
+            self.setCo2Levels(int(dayReading))
